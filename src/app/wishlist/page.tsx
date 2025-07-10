1f7641634f70e69b89fe3eaa7d14bfea9ca16c29
Abasiakan Mbat <basky2592@gmail.com>
2025-07-10 21:19:08 +0100

    feat: enhance wishlist page with back to home link and improved empty state message

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -2,6 +2,8 @@
 import { motion } from 'framer-motion'
 import { useRoomStore } from '@/store/roomstore'
 import RoomCard from '@/components/common/roomCard'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function WishlistPage() {
   const rooms = useRoomStore((state) => state.rooms)
@@ -29,9 +31,33 @@ export default function WishlistPage() {
             <RoomCard key={room.id} {...room} toggleLike={toggleLike} />
           ))
         ) : (
-          <p className="text-center w-full text-gray-500">No liked items yet.</p>
+          <p className=" w-full text-gray-500">No liked items yet. 
+          <Link href="/" className="text-blue-500 hover:underline translate-y-1/2 pl-4">
+            Go back to home
+          </Link>
+          </p>
         )}
+     
+
+
+     
       </motion.div>
+
+      <Link
+      href="/"
+      className="fixed z-50 bottom-10 right-10 flex items-center gap-2 bg-white shadow-lg border hover:shadow-xl transition-all duration-300 ease-in-out rounded-full px-5 py-2.5 hover:scale-105 hover:bg-red-50 group"
+    >
+      <span className="text-sm font-semibold text-gray-800 group-hover:text-red-500 transition-colors duration-300">
+        Back to Home
+      </span>
+      <Image
+        src="/red-heart.svg"
+        alt="Wishlist"
+        width={20}
+        height={20}
+        className="group-hover:scale-115 transition-transform duration-300"
+      />
+    </Link>
     </div>
   )
 }
